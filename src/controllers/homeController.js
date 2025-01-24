27d4942f2fe69a782da553b32f4691d11d6c9c50
LuisCurzi22 <Luiscurzi22@gmail.com>
2025-01-24 00:26:19 -0300

    actualizacion sprint3

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,6 +7,18 @@ const homeController = {
         res.render('home.ejs', {
             titulo: 'UrbanSteps'
         })
+    },
+    about: (req, res) => {
+        res.render('acerca-de.ejs')
+    },
+    preguntas: (req, res) => {
+        res.render('preguntas-f.ejs')
+    },
+    talles: (rep, res) => {
+        res.render('guia-talles.ejs')
+    },
+    termino: (rep, res) =>{
+        res.render('tyc.ejs')
     }
 }
 module.exports = homeController
--- src/routes/routeHome.js
+++ src/routes/routeHome.js
@@ -4,4 +4,8 @@ const router = express.Router()
 const homeController = require('../controllers/homeController.js')
 
 router.get('/', homeController.index)
+router.get('/acerca-de', homeController.about)
+router.get('/preguntas-f', homeController.preguntas)
+router.get('/guia-talles',homeController.talles)
+router.get('/tyc', homeController.termino)
 module.exports = router
