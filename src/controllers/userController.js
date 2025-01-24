d89c688158041b31098eec53060b3594382e85a2
CurziLucas <lucasantoniocurzi@gmail.com>
2025-01-24 00:15:39 -0300

    sprint3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,6 +8,15 @@ const userController = {
     }, 
     register: (req, res) => {
         res.render('users/register.ejs')
+    },
+    perfil: (req, res) => {
+        res.render('users/perfil.ejs')
+    },
+    editarperfil: (req, res) => {
+        res.render('users/editarperfil.ejs')
+    },
+    admin: (req, res) => {
+        res.render('users/admin.ejs')
     }
 }
 module.exports = userController
--- src/routes/routeUser.js
+++ src/routes/routeUser.js
@@ -5,4 +5,7 @@ const userController = require('../controllers/userController.js')
 
 router.get('/login', userController.login)
 router.get('/register', userController.register)
+router.get('/perfil', userController.perfil)
+router.get('/editarperfil', userController.editarperfil)
+router.get('/admin', userController.admin)
 module.exports = router
